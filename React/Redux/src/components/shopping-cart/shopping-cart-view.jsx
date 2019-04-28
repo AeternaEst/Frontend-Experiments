a91ac2b159e6e1eedae16ce15e3a2ea9ca222c2a
Nicholas Lindboe <nicholaslindboe@gmail.com>
2019-04-28 17:24:41 +0200

    Async API with correct error handling

--- React/Redux/src/components/shopping-cart/shopping-cart-view.jsx
+++ React/Redux/src/components/shopping-cart/shopping-cart-view.jsx
@@ -2,28 +2,16 @@ import { connect } from 'react-redux'
 import ShoppingCartApp from './shopping-cart-app';
 import { addToCart, removeFromCart } from './../../actions/shopping-cart/shopping-cart-actions';
 
-const texts = {
-    counterText: "Number of items",
-    cartOverviewTexts: {
-        name: "Product",
-        price: "Price",
-        quantity: "Quantity",
-        removeProduct: "Remove",
-        total: "Total"
-    }
-}
-
 function mapStateToProps(state) {
     console.log("Mapping state to props");
     console.log(state);
     return {
         products: state.products,
         showError: state.error.errorDetected,
-        errorMessage: state.error.errorMessage,
-        counterMessage: texts.counterText,
+        errorMessage: state.error.errorMessage,       
         numOfItemsInCart: state.cart.length,
-        cart: state.cart,
-        cartOverviewLabels: texts.cartOverviewTexts
+        cart: state.cart,     
+        isLoading: state.waitingForServerResponse 
     }
 }
 
