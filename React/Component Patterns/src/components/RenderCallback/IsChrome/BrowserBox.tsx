053d24be4af74ce75f5ee91f761b06d977c6cda5
Nicholas Lindboe <nicholaslindboe@gmail.com>
2019-11-08 17:39:14 +0100

    fixed type versions for react

--- React/Component Patterns/src/components/RenderCallback/IsChrome/BrowserBox.tsx
+++ React/Component Patterns/src/components/RenderCallback/IsChrome/BrowserBox.tsx
@@ -2,15 +2,15 @@ import React, { FunctionComponent } from 'react';
 import IdentifyChrome from './IdentifyChrome';
 
 const BrowserBox: FunctionComponent = (props: any) => (
-    <IdentifyChrome>
-        {(isChromeBrowser) => (
-            isChromeBrowser ? (
-                <div className="chrome-box">Hello Chrome</div>
-            ) : (
-                <div className="not-chrome-box">This is not chrome</div>
-            )
-        )} 
-    </IdentifyChrome>    
-)
+  <IdentifyChrome>
+    {isChromeBrowser => (
+      isChromeBrowser ? (
+        <div className="chrome-box">Hello Chrome</div>
+      ) : (
+        <div className="not-chrome-box">This is not chrome</div>
+      )
+    )}
+  </IdentifyChrome>
+);
 
 export default BrowserBox;
