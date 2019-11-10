9a3c2f08c5e14867c0c7427e4a5ca16cb615aa70
Nicholas Lindboe <nicholaslindboe@gmail.com>
2019-11-10 18:18:25 +0100

    added normal and generic typeguard

--- Typescript/Basic/src/app.ts
+++ Typescript/Basic/src/app.ts
@@ -1,8 +1,9 @@
-import constraints from "./generics/constraints";
-
+//import typeGuards from "./types/type-guards";
+import genericTypeGuard from "./generics/type-guards";
+//import constraints from "./generics/constraints";
 //import genericInterface from "./generics/interface";
 //import animals from './types/casting';
 //import automap from './classes/automap';
 //import assertion from './types/assertion';
 
-constraints();
+genericTypeGuard();
---
+++ Typescript/Basic/src/generics/type-guards.ts
@@ -0,0 +1,33 @@
+
+interface Institution {
+  name: string;
+}
+
+class Bank implements Institution {
+  constructor(public name: string, public amountOfMoney: number) {}
+}
+
+class University implements Institution {
+  constructor(public name: string, public address: string) {}
+}
+
+function isOfType<T>(type: any, property: keyof T): type is T {
+  return (type as T)[property] !== undefined;
+}
+
+const ofType = <T>(type: any, property: keyof T): type is T => (type as T)[property] !== undefined;
+
+function genericTypeGuard(): void {
+  const bank = new Bank("Danske Bank", 10000000) as Institution;
+  const university = new University("CBS", "Bredgade 18") as Institution;
+
+  if(isOfType<Bank>(bank, "amountOfMoney")) {
+    console.log(`${bank.name} owns ${bank.amountOfMoney} amount of money`)
+  }
+
+  if(ofType<University>(university, "address")) {
+    console.log(`${university.name} are located on ${university.address}`)
+  }
+}
+
+export default genericTypeGuard;
