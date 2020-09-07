fda6c813686b74109d67052b61865a5efe1021b1
nli <nli@magnetix.dk>
2020-09-07 16:03:00 +0200

    Setup redux devtool

--- React/Redux-Saga/src/setup.ts
+++ React/Redux-Saga/src/setup.ts
@@ -1,4 +1,5 @@
 import { rootReducer } from "./reducers/root-reducer";
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
