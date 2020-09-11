766ef1cce15f9a774c5130592c9c3b02aa1b877b
Nicholas Lindboe <NicholasLindboe@gmail.com>
2020-09-11 15:41:18 +0200

    add redux devtools to saga setup

--- React/Redux-Saga/src/setup.ts
+++ React/Redux-Saga/src/setup.ts
@@ -1,12 +1,13 @@
 import { rootReducer } from "./reducers/root-reducer";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "@redux-saga/core";
 import deprecatedSaga from "./sagas/app-setup/deprecated-saga";
 
-const devTools = composeWithDevTools();
 const sagaMiddleware = createSagaMiddleware()
 
-export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+export const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(sagaMiddleware),
+  ));
 
 sagaMiddleware.run(deprecatedSaga);
