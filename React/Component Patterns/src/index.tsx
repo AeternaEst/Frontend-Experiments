3313de64e69dda4be5cd7fdb0bac5ac58865335d
nli <nli@magnetix.dk>
2021-02-05 11:13:37 +0100

    untrack removed files from git

--- React/Component Patterns/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import RerenderFunction from "./performance/rerender-function/RerenderFunction";
-
-const injectTarget = document.querySelector(".root");
-
-ReactDOM.render(<RerenderFunction />, injectTarget);
