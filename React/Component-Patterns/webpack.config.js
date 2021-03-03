a9e1aeb7a1546129baee09f4f9327dc3d803071b
nli <nli@magnetix.dk>
2021-03-03 14:14:28 +0100

    initial setup of lint-staged

--- React/Component-Patterns/webpack.config.js
+++ React/Component-Patterns/webpack.config.js
@@ -1,39 +1,25 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// const ESLintPlugin = require("eslint-webpack-plugin");
-
 module.exports = {
   mode: "development",
   entry: ["./src/index.tsx"],
   devtool: "inline-source-map",
   watchOptions: {
-    ignored: /node_modules/
+    ignored: /node_modules/,
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["ts-loader"]
-      }
-    ]
+        use: ["ts-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"]
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
   },
   output: {
     path: `${__dirname}/dist`,
     publicPath: "/",
-    filename: "bundle.js"
-  }
+    filename: "bundle.js",
+  },
 };
-
-// plugins: [
-//   new ESLintPlugin({
-//     context: __dirname,
-//     eslintPath: `${__dirname}/node_modules/.bin/eslint`,
-//     extensions: ["ts", "tsx"],
-//     files: "./src/**",
-//     fix: true,
-//     exclude: "node_modules"
-//   })
-// ]
