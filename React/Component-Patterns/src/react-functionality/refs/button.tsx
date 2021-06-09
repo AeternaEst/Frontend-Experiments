9f0c0cc277c5da6b7813ff8ae9db7b0f8fcaf17e
nli <nli@magnetix.dk>
2021-06-09 10:22:33 +0200

    add global validate function to form

--- React/Component-Patterns/src/react-functionality/refs/button.tsx
+++ React/Component-Patterns/src/react-functionality/refs/button.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const Button = React.forwardRef((_props, ref: React.Ref<HTMLButtonElement>) => (
+interface ButtonProps {
+  text: string;
+}
+
+const Button = (props: ButtonProps, ref: React.Ref<HTMLButtonElement>) => (
   <button
     ref={ref}
     onFocus={() => console.log("button received focus")}
     type="button"
     className="button"
   >
-    Hello
+    {props.text}
   </button>
-));
+);
 
-export default Button;
+export default React.forwardRef<HTMLButtonElement, ButtonProps>(Button);
